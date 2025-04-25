6d0eecafdd1eb6aae2b3cbc00e4eb38fad28dafd
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-04-25 14:42:08 +0900

    feat: responsive design to BandProjectCards

--- src/App.tsx
+++ src/App.tsx
@@ -11,50 +11,52 @@ export default function App() {
   return (
     <div className="flex flex-col items-center justify-center bg-white dark:bg-black">
       <Banner />
-      <div className="flex flex-col items-center justify-center p-10 my-10 space-y-20 bg-white dark:bg-black">
-        <BandProjectCard
-          subTitle="1st project"
-          description={
-            <>
-              우당탕탕 <strong>락스타</strong>를 꿈꾼다면? <br />
-              첫만남 밴드 매칭 프로젝트
-            </>
-          }
-          title="BANDAY"
-          image={[bandayImg]}
-        />
-        <BandProjectCard
-          subTitle="2nd project"
-          description={
-            <>
-              어쿠스틱한 잔잔함이 좋다면? <br />
-              <strong>어쿠스틱</strong> 밴드 프로젝트
-            </>
-          }
-          title="Bcoustic"
-          image={[bcousticImg]}
-        />
-        <BandProjectCard
-          subTitle="3rd project"
-          description={
-            <>
-              연주는 못하지만 밴드는 하고 싶어 <br />
-              <strong>왕초보</strong> 밴드 프로젝트
-            </>
-          }
-          title="NEWB Band"
-          image={[newBImg]}
-        />
-        <BandProjectCard
-          subTitle="Special project"
-          description={
-            <>
-              일상밴드 회원 누구나 편하게 <br />
-            </>
-          }
-          title="Friendship"
-          image={[friendshipImg1, friendshipImg2, friendshipImg3]}
-        />
+      <div className="flex flex-col items-center w-full p-10 my-10 bg-white dark:bg-black">
+        <div className="w-full items-center justify-center flex flex-wrap gap-20 max-w-[2000px]">
+          <BandProjectCard
+            subTitle="1st project"
+            description={
+              <>
+                우당탕탕 <strong>락스타</strong>를 꿈꾼다면? <br />
+                첫만남 밴드 매칭 프로젝트
+              </>
+            }
+            title="BANDAY"
+            image={[bandayImg]}
+          />
+          <BandProjectCard
+            subTitle="2nd project"
+            description={
+              <>
+                어쿠스틱한 잔잔함이 좋다면? <br />
+                <strong>어쿠스틱</strong> 밴드 프로젝트
+              </>
+            }
+            title="Bcoustic"
+            image={[bcousticImg]}
+          />
+          <BandProjectCard
+            subTitle="3rd project"
+            description={
+              <>
+                연주는 못하지만 밴드는 하고 싶어 <br />
+                <strong>왕초보</strong> 밴드 프로젝트
+              </>
+            }
+            title="NEWB Band"
+            image={[newBImg]}
+          />
+          <BandProjectCard
+            subTitle="Special project"
+            description={
+              <>
+                일상밴드 회원 누구나 편하게 <br />
+              </>
+            }
+            title="Friendship"
+            image={[friendshipImg1, friendshipImg2, friendshipImg3]}
+          />
+        </div>
       </div>
     </div>
   );
